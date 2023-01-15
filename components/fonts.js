f0f42e9be16f2f7424f8277e91d0858953382978
Francesco Barbieri <fbarbieri.business@gmail.com>
2023-01-15 17:59:35 +0100

    reinizialised repo plus navbar and basic routing

---
+++ components/fonts.js
@@ -0,0 +1,8 @@
+import { Roboto_Slab, Source_Sans_Pro } from '@next/font/google'
+
+export const robotoSlab = Roboto_Slab({subsets: ['latin']})
+
+export const sourceSans = Source_Sans_Pro({
+    weight: ['400'],
+    subsets: ['latin']
+})
