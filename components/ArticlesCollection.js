ff8518a535af4e73dbbc55c9076dbf06b82ece7d
Francesco Barbieri <fbarbieri.business@gmail.com>
2023-01-24 21:36:15 +0100

    some changes on archive page [itemCollection.js]

--- components/ArticlesCollection.js
+++ components/ArticlesCollection.js
@@ -1,45 +1,43 @@
 import Article from "./Article";
-import styles from "../styles/ArticlesCollection.module.css"
+import styles from "../styles/ArticlesCollection.module.css";
+import { Pagination } from "@mui/material";
+import { useState } from "react";
 
-const ArticlesCollection = () => {
+const ArticlesCollection = ({articles, pages}) => {
+    const [page, setPage] = useState(1);
 
-    const articles = [
-        {
-            id: "articleID",
-            date: "december 12, 1291",
-            tag: "general",
-            title: "Getting your data in shape for machine learning",
-            preview: "Machine learning uses data structures that don't always resemble the ones used in standard computing. You'll need to process your data first if you want efficient machine learning.",
-            mainImg: "https://firebasestorage.googleapis.com/v0/b/francescobarbieri-73605.appspot.com/o/maxresdefault.jpg?alt=media"
-        },
-        {
-            id: "articleID",
-            date: "december 12, 2056",
-            tag: "general",
-            title: "The Winter/Summer Bash 2022 Hat Cafe is now closed!",
-            preview: "We had fun celebrating Winter Bash 2022 with you all! While we’ve closed our cafe, let’s look at a few highlights and hat-wearing avatars that brought us joy this holiday season.",
-            mainImg: "https://firebasestorage.googleapis.com/v0/b/francescobarbieri-73605.appspot.com/o/maxresdefault.jpg?alt=media"
-        },
-        {
-            id: "articleID",
-            date: "december 12, 3085",
-            tag: "general",
-            title: "Stack Gives Back 2022!",
-            preview: "Let’s start the year on a high note! We’re excited to announce our 14th Stack Gives Back.",
-            mainImg: "https://firebasestorage.googleapis.com/v0/b/francescobarbieri-73605.appspot.com/o/maxresdefault.jpg?alt=media"
-        },
+    const changePage = (event, value) => {
+        setPage(value);
+    }
 
-    ]
+    if(pages)
+    {
+        const currentPage = 1;
+        const pagesNumber = Math.ceil(Object.keys(articles).length / 12);
 
-    return (
-        <div className={styles.articlesCollection}>
-            {
-                articles.map( article => (
-                    <Article key={article.id} articleData={article} />
-                ))
-            }
-        </div>
-    );
+        return (
+            <div className={styles.paginationContainer}>
+                <Pagination
+                    count={pagesNumber}
+                    variant="outlined"
+                    shape="rounded"
+                    page={page}
+                    onChange={changePage}
+                />
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className={styles.articlesCollection}>
+                {
+                    articles.map( article => (
+                        <Article key={article.id} articleData={article} />
+                    ))
+                }
+            </div>
+        );
+    }
 }
  
 export default ArticlesCollection;
